a6037d47584a9ca75b401db2be6d20a73304c1df
Teddy <dudetotheleft@gmail.com>
2023-07-18 16:29:29 -0700

    Created Student and Advisor classes

---
+++ Advisor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an individual college advisor
+    /// </summary>
+    public class Advisor
+    {
+        /// <summary>
+        /// Legal first and legal last names
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Assigned work email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Building and room number of work office
+        /// </summary>
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// Preferred phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Assigned school advisor
+        /// </summary>
+        public Advisor AssignedAdvisor { get; set; }
+    }
+}
